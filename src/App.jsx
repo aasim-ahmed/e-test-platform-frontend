ca33955f357b6c83373add0029d8f9e84b22bbaa
aasim-ahmed <theaasim22@gmail.com>
2025-05-13 16:48:18 +0530

    this is my Frontend updated code

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,28 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route ,Link} from 'react-router-dom'
 import Home from './pages/Home.jsx' 
+import Layout from './components/Layout.jsx';
 import Navbar from './components/Navbar.jsx'
 import GenerateQuestion from './pages/GenerateTest/GenerateQuestion.jsx'
+import TestGenerator from './pages/GenerateTest/GenerateTest.jsx'
+import Signup from './features/auth/Signup.jsx'
+import Login from './features/auth/Login.jsx'
 
 export default function App() {
   return (
     <>
     <Navbar/>
-    <br />
-    <br /><br />
-    <GenerateQuestion/>
-    {/* <Home/> */}
-      {/* <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/test" element={<Test />} />
-          <Route path="/result" element={<Result />} />
-          <Route path="/questionlist" element={<QuestionList />} />
-          <Route path="/admin" element={<Admin />} />
-          <Route path="/candidate" element={<Candidate />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter> */}
-      
+    <Layout>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/generate-question' element={<GenerateQuestion/>}/>
+     <Route path='/generate-test' element={<TestGenerator/>}/>
+    < Route path='/Signup' element={<Signup/>}/>
+    < Route path='/login' element={<Login/>}/>
+    
+    </Routes> 
+    </Layout>
+    
       
     </>
   )

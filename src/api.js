eb4cfc121eb481fc6abe24c45798fd2cfa871d04
aasim-ahmed <theaasim22@gmail.com>
2025-05-15 18:00:55 +0530

    My todays work

---
+++ src/api.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+// Create instance
+const api = axios.create({
+  baseURL: 'http://localhost:5500', // Change to your backend URL
+});
+
+// Request interceptor to attach token
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default api;

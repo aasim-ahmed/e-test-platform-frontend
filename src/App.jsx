a877a67d7baab0bff482af6a61f0d38fc4e93f1e
aasim-ahmed <theaasim22@gmail.com>
2025-05-15 18:01:30 +0530

    My todays work

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,72 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route ,Link} from 'react-router-dom'
-import Home from './pages/Home.jsx' 
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.jsx';
 import Layout from './components/Layout.jsx';
-import Navbar from './components/Navbar.jsx'
-import GenerateQuestion from './pages/GenerateTest/GenerateQuestion.jsx'
-import TestGenerator from './pages/GenerateTest/GenerateTest.jsx'
-import Signup from './features/auth/Signup.jsx'
-import Login from './features/auth/Login.jsx'
+import Navbar from './components/Navbar.jsx';
+import GenerateQuestion from './pages/GenerateTest/GenerateQuestion.jsx';
+import TestGenerator from './pages/GenerateTest/GenerateTest.jsx';
+import Signup from './features/auth/Signup.jsx';
+import Login from './features/auth/Login.jsx';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Dashboard from './pages/Dashboard/Dashboard.jsx';
+import Admin from './pages/Dashboard/Admin.jsx';
+import Recruiter from './pages/Dashboard/Recruiter.jsx';
+import TechnicalExpert from './pages/Dashboard/TechnicalExpert.jsx';
+import Candidate from './pages/Dashboard/candidate.jsx';
+import ProtectedRoute from './ProtectedRoute.jsx';
 
 export default function App() {
   return (
-    <>
-    <Navbar/>
-    <Layout>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/generate-question' element={<GenerateQuestion/>}/>
-     <Route path='/generate-test' element={<TestGenerator/>}/>
-    < Route path='/Signup' element={<Signup/>}/>
-    < Route path='/login' element={<Login/>}/>
-    
-    </Routes> 
-    </Layout>
-    
-      
-    </>
-  )
+    <BrowserRouter>
+      <Navbar />
+      <ToastContainer position="top-right" autoClose={3000} />
+      <Layout>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/generate-question' element={<GenerateQuestion />} />
+          <Route path='/generate-test' element={<TestGenerator />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+
+          <Route
+            path='/candidate'
+            element={
+              <ProtectedRoute allowedRoles={['candidate']}>
+                <Candidate />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/admin'
+            element={
+              <ProtectedRoute allowedRoles={['admin']}>
+                <Admin />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path='/recruiter'
+            element={
+              <ProtectedRoute allowedRoles={['recruiter']}>
+                <Recruiter />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path='/technical-expert'
+            element={
+              <ProtectedRoute allowedRoles={['technical_expert']}>
+                <TechnicalExpert />
+              </ProtectedRoute>
+            }
+          />
+
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
